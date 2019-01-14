a7c56fa8071f4dec85c7194c81d901b2c55966d3
DelSquared <37172200+DelSquared@users.noreply.github.com>
2019-01-14 23:44:21 +0100

    fixed an error

--- python/main.py
+++ python/main.py
@@ -6,7 +6,8 @@ sgm=0
 n=0
 n2=n*n
 
-N=150
+N=1000
+
 Z=range(N)
 
 for _ in Z:
@@ -18,10 +19,10 @@ for _ in Z:
 
 fig=plt.figure()
 plt.plot(Z,sigma,label=r'$\sigma(n)$')
-plt.plot(Z,np.sqrt(Z)+np.ones_like(Z),label=r'$1+\sqrt{x}$')
-plt.plot(Z,np.sqrt(Z)+np.ones_like(Z)/2,label=r'$\frac{1}{2}+\sqrt{x}}$')
-plt.plot(Z,np.sqrt(Z),label=r'$0+\sqrt{x}$')
-plt.plot(Z,np.append([1],1/(np.sqrt(Z[1:])+np.ones_like(Z[1:])/2)),label=r'$\rho_\sigma (x)$')
+plt.plot(Z,np.sqrt(Z)+np.ones_like(Z),label=r'$\sqrt{x}+1$')
+plt.plot(Z,(np.sqrt(Z)+np.sqrt(Z+np.ones_like(Z))+np.ones_like(Z))/2,label=r'$\frac{\sqrt{x}+1+\sqrt{x+1}}{2}$')
+plt.plot(Z,np.sqrt(Z+np.ones_like(Z)),label=r'$\sqrt{x+1}$')
+plt.plot(Z,np.append([1],1/((np.sqrt(Z[1:])+np.sqrt(Z[1:]+np.ones_like(Z[1:]))+np.ones_like(Z[1:]))/2)),label=r'$\rho_\sigma (x)$')
 plt.grid()
 plt.legend()
 plt.savefig("plot.png")
