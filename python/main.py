68373c4944b67667bcd044652eccaa297cb4f934
DelSquared <37172200+DelSquared@users.noreply.github.com>
2019-01-14 23:23:08 +0100

    Create main.py

---
+++ python/main.py
@@ -0,0 +1,28 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+sigma = np.array([])
+sgm=0
+n=0
+n2=n*n
+
+N=150
+Z=range(N)
+
+for _ in Z:
+  if _==n2:
+    sgm+=1
+    n+=1
+    n2=n*n
+  sigma=np.append(sigma,[sgm])
+
+fig=plt.figure()
+plt.plot(Z,sigma,label=r'$\sigma(n)$')
+plt.plot(Z,np.sqrt(Z)+np.ones_like(Z),label=r'$1+\sqrt{x}$')
+plt.plot(Z,np.sqrt(Z)+np.ones_like(Z)/2,label=r'$\frac{1}{2}+\sqrt{x}}$')
+plt.plot(Z,np.sqrt(Z),label=r'$0+\sqrt{x}$')
+plt.plot(Z,np.append([1],1/(np.sqrt(Z[1:])+np.ones_like(Z[1:])/2)),label=r'$\rho_\sigma (x)$')
+plt.grid()
+plt.legend()
+plt.savefig("plot.png")
+plt.close(fig)
